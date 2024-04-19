4f06d74b70709d367e2c99c0109a8a01ac77c5c4
Dogancan Kınık <dogancankinik@gmail.com>
2024-04-19 14:26:43 +0300

    Update ResultAnalyzer.java

--- src/test/java/com/workintech/sqlintro/ResultAnalyzer.java
+++ src/test/java/com/workintech/sqlintro/ResultAnalyzer.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 public class ResultAnalyzer implements TestWatcher, AfterAllCallback{
     private List<TestResultStatus> testResultsStatus = new ArrayList<>();
-    private static final String taskId = "155";
+    private static final String taskId = "157";
 
     private enum TestResultStatus {
         SUCCESSFUL, ABORTED, FAILED, DISABLED;
